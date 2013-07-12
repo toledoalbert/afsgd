287ee5d94a66f67d95b178467d6f8142e212d1f1
Albert Toledo <toledoalbert@gmail.com>
2013-07-11 23:57:55 -0400

    footer changed

--- application/views/site_footer.php
+++ application/views/site_footer.php
@@ -4,51 +4,43 @@
         <div class="span12">
           <div class="span2" style="width: 15%;">
             <ul class="unstyled">
-              <li>GitHub<li>
-              <li><a href="#">About us</a></li>
-              <li><a href="#">Blog</a></li>
-              <li><a href="#">Contact & support</a></li>
-              <li><a href="#">Enterprise</a></li>
-              <li><a href="#">Site status</a></li>
+              <li>DERNEK<li>
+              <li><a href="#">Hakkimizda</a></li>
+              <li><a href="#">Nasil Uye Olunur?</a></li>
+              <li><a href="#">Merkez Yonetim Kurulu</a></li>
             </ul>
           </div>
           <div class="span2" style="width: 15%;">
             <ul class="unstyled">
-              <li>Applications<li>
-              <li><a href="#">Product for Mac</a></li>
-              <li><a href="#">Product for Windows</a></li>
-              <li><a href="#">Product for Eclipse</a></li>
-              <li><a href="#">Product mobile apps</a></li>              
+              <li>DUYURULAR<li>
+              <li><a href="#">MYK</a></li>
+              <li><a href="#">Istanbul Subesi</a></li>
+              <li><a href="#">Izmir Subesi</a></li>
+              <li><a href="#">Ankara Subesi</a></li>              
             </ul>
           </div>
           <div class="span2" style="width: 15%;">
             <ul class="unstyled">
-              <li>Services<li>
-              <li><a href="#">Web analytics</a></li>
-              <li><a href="#">Presentations</a></li>
-              <li><a href="#">Code snippets</a></li>
-              <li><a href="#">Job board</a></li>              
+              <li>PROJELER<li>
+              <li><a href="#">Kamplar</a></li>
+              <li><a href="#">Shapex</a></li>
+              <li><a href="#">AFS'D</a></li>
+              <li><a href="#">Fest-i Kult</a></li>              
             </ul>
           </div>
           <div class="span2" style="width: 15%;">
             <ul class="unstyled">
-              <li>Documentation<li>
-              <li><a href="#">Product Help</a></li>
-              <li><a href="#">Developer API</a></li>
-              <li><a href="#">Product Markdown</a></li>
-              <li><a href="#">Product Pages</a></li>              
+              <li>ADRES<li>
+              <li>Samanyolu Sokak, <br> Osmanbey Istanbul</li>              
             </ul>
-          </div>  
+          </div>
           <div class="span2" style="width: 15%;">
             <ul class="unstyled">
-              <li>More<li>
-              <li><a href="#">Training</a></li>
-              <li><a href="#">Students & teachers</a></li>
-              <li><a href="#">The Shop</a></li>
-              <li><a href="#">Plans & pricing</a></li>
-              <li><a href="#">Contact us</a></li>
+              <li>Tel: 0212 xxx xxxx<li>
+              <li>Samanyolu Sokak, <br> Osmanbey Istanbul</li>              
             </ul>
-          </div>          
+          </div>
+          
         </div>
       </div>
       <hr>
