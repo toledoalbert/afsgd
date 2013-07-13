d4df5c13150971e1f33bf92c955cd69d674c49a4
Albert Toledo <toledoalbert@gmail.com>
2013-07-12 22:58:54 -0400

    Navbar changed logo added

---
+++ application/views/great_nav.php
@@ -0,0 +1,129 @@
+<div class="container">
+<div> <!-- MENU BEGIN -->
+              <label class="mobile_menu" for="mobile_menu">
+              <span>Menu</span>
+              </label>
+              <input id="mobile_menu" type="checkbox">
+              <ul class="nav">
+              <li><a href="#"><i class="icon-home icon-large"></i></a></li>
+              <li class="dropdown"><a href="#">Markets</a>
+              <div class="fulldrop">
+                <div class="column">
+                  <h3>Codecanyon</h3>
+                  <ul>
+                    <li><a href="#">PHP Scripts</a></li>
+                    <li><a href="#">Wordpress Plugins</a></li>
+                    <li><a href="#">HTML &amp; CSS</a></li>
+                    <li><a href="#">More</a></li>
+                  </ul>
+                </div>
+                <div class="column">
+                  <h3>Graphicriver</h3>
+                  <ul>
+                    <li><a href="#">Vectors</a></li>
+                    <li><a href="#">Icons</a></li>
+                    <li><a href="#">User Interface</a></li>
+                    <li><a href="#">More</a></li>
+                  </ul>
+                </div>
+                <div class="column">
+                  <h3>Themeforest</h3>
+                  <ul>
+                    <li><a href="#">HTML Templates</a></li>
+                    <li><a href="#">Wordpress Themes</a></li>
+                    <li><a href="#">PSD Templates</a></li>
+                    <li><a href="#">More</a></li>
+                  </ul>
+                </div>
+                <div class="column">
+                  <h3>3docean</h3>
+                  <ul>
+                    <li><a href="#">3D Models</a></li>
+                    <li><a href="#">Materials</a></li>
+                    <li><a href="#">Textures</a></li>
+                    <li><a href="#">More</a></li>
+                  </ul>
+                </div>
+                <div class="column">
+                  <h3>Audiojungle</h3>
+                  <ul>
+                    <li><a href="#">Music</a></li>
+                    <li><a href="#">Sounds</a></li>
+                    <li><a href="#">Source Files</a></li>
+                    <li><a href="#">More</a></li>
+                  </ul>
+                </div>
+                <div class="column">
+                  <h3>Videohive</h3>
+                  <ul>
+                    <li><a href="#">Apple Motion</a></li>
+                    <li><a href="#">Stock Footage</a></li>
+                    <li><a href="#">Cinema 4D</a></li>
+                    <li><a href="#">More</a></li>
+                  </ul>
+                </div>
+              </div>
+              </li>
+              <li class="dropdown"><a href="#">Envato</a>
+                <ul>
+                  <li><a href="#">Video</a></li>
+                  <li><a href="#">Audio</a></li>
+                  <li><a href="#">Design</a></li>
+                  <li class="dropdown"><a href="#">Web</a>     
+                    <ul>
+                      <li><a href="#">HTML</a></li>
+                      <li><a href="#">CSS</a></li>
+                      <li class="dropdown"><a href="#">Templates</a>
+                        <ul>
+                          <li><a href="#">Wordpress</a></li>
+                          <li><a href="#">Joomla</a></li>
+                          <li><a href="#">Drupal</a></li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </li>
+                                    <li><a href="#">Audio</a></li>
+                  <li><a href="#">Design</a></li>
+                </ul>
+              </li>
+              <li class="dropdown"><a href="#">Tutorials</a>
+              <div class="coldrop">
+                <div class="column">
+                  <h3>+ PSDtuts</h3>
+                  <ul>
+                    <li><a href="#">Tutorials</a></li>
+                    <li><a href="#">Articles</a></li>
+                    <li><a href="#">Tips</a></li>
+                    <li><a href="#">Videos</a></li>
+                  </ul>
+                </div>
+                <div class="column">
+                  <h3>+ NETtuts</h3>
+                  <ul>
+                    <li><a href="#">Tutorials</a></li>
+                    <li><a href="#">Articles</a></li>
+                    <li><a href="#">Tips</a></li>
+                    <li><a href="#">Videos</a></li>
+                  </ul>
+                </div>
+                <div class="column">
+                  <h3>+ WPtuts</h3>
+                  <ul>
+                    <li><a href="#">Tutorials</a></li>
+                    <li><a href="#">Articles</a></li>
+                    <li><a href="#">Tips</a></li>
+                    <li><a href="#">Videos</a></li>
+                  </ul>
+                </div>
+              </div>
+              </li>
+              <li><a href="#"><i class="icon-cloud-upload icon-large"></i> Upload</a></li>
+              <li class="search">
+                  <form action="#">
+                  <input type="text" placeholder="Search">
+                  </form>
+              </li>
+          </ul> 
+
+        </div>
+        </div>
--- application/views/horBlue_nav.php
+++ application/views/horBlue_nav.php
@@ -1,43 +1,21 @@
 <nav id="cbp-hrmenu" class="cbp-hrmenu">
+					<div class="nav-right">
+						<a href="#"><button class="btn btn-primary">Gonullu Portali</button></a>
+					</div>
 					<ul style="position: relative; z-index: 1;">
-						<li><a class="brand" href="#">AFSGD</a></li>
+						<li><a style="padding-left: 40px;" class="brand" href="#">AFSGD</a></li>
 						<li>
-							<a href="#">Products</a>
+							<a href="#">Dernek</a>
 							<div class="cbp-hrsub">
 								<div class="cbp-hrsub-inner"> 
 									<div>
-										<h4>Learning &amp; Games</h4>
-										<ul>
-											<li><a href="#">Catch the Bullet</a></li>
-											<li><a href="#">Snoopydoo</a></li>
-											<li><a href="#">Fallen Angel</a></li>
-											<li><a href="#">Sui Maker</a></li>
-											<li><a href="#">Wave Master</a></li>
-											<li><a href="#">Golf Pro</a></li>
-										</ul>
+										<a>Hakkimizda<a>
 									</div>
 									<div>
-										<h4>Utilities</h4>
-										<ul>
-											<li><a href="#">Gadget Finder</a></li>
-											<li><a href="#">Green Tree Express</a></li>
-											<li><a href="#">Green Tree Pro</a></li>
-											<li><a href="#">Wobbler 3.0</a></li>
-											<li><a href="#">Coolkid</a></li>
-										</ul>
+										<a>Nasil Uye Olunur?</a>
 									</div>
 									<div>
-										<h4>Education</h4>
-										<ul>
-											<li><a href="#">Learn Thai</a></li>
-											<li><a href="#">Math Genius</a></li>
-											<li><a href="#">Chemokid</a></li>
-										</ul>
-										<h4>Professionals</h4>
-										<ul>
-											<li><a href="#">Success 1.0</a></li>
-											<li><a href="#">Moneymaker</a></li>
-										</ul>
+										<a>Merkezi Yonetim</a>
 									</div>
 								</div><!-- /cbp-hrsub-inner -->
 							</div><!-- /cbp-hrsub -->
@@ -45,41 +23,15 @@
 						<li>
 							<a href="#">Downloads</a>
 							<div class="cbp-hrsub">
-								<div class="cbp-hrsub-inner">
+								<div class="cbp-hrsub-inner"> 
 									<div>
-										<h4>Education &amp; Learning</h4>
-										<ul>
-											<li><a href="#">Learn Thai</a></li>
-											<li><a href="#">Math Genius</a></li>
-											<li><a href="#">Chemokid</a></li>
-										</ul>
-										<h4>Professionals</h4>
-										<ul>
-											<li><a href="#">Success 1.0</a></li>
-											<li><a href="#">Moneymaker</a></li>
-										</ul>
+										<a>Hakkimizda<a>
 									</div>
 									<div>
-										<h4>Entertainment</h4>
-										<ul>
-											<li><a href="#">Gadget Finder</a></li>
-											<li><a href="#">Green Tree Express</a></li>
-											<li><a href="#">Green Tree Pro</a></li>
-											<li><a href="#">Holy Cannoli</a></li>
-											<li><a href="#">Wobbler 3.0</a></li>
-											<li><a href="#">Coolkid</a></li>
-										</ul>
+										<a>Nasil Uye Olunur?</a>
 									</div>
 									<div>
-										<h4>Games</h4>
-										<ul>
-											<li><a href="#">Catch the Bullet</a></li>
-											<li><a href="#">Snoopydoo</a></li>
-											<li><a href="#">Fallen Angel</a></li>
-											<li><a href="#">Sui Maker</a></li>
-											<li><a href="#">Wave Master</a></li>
-											<li><a href="#">Golf Pro</a></li>
-										</ul>
+										<a>Merkezi Yonetim</a>
 									</div>
 								</div><!-- /cbp-hrsub-inner -->
 							</div><!-- /cbp-hrsub -->
@@ -89,32 +41,13 @@
 							<div class="cbp-hrsub">
 								<div class="cbp-hrsub-inner"> 
 									<div>
-										<h4>Learning &amp; Games</h4>
-										<ul>
-											<li><a href="#">Catch the Bullet</a></li>
-											<li><a href="#">Snoopydoo</a></li>
-										</ul>
-										<h4>Utilities</h4>
-										<ul>
-											<li><a href="#">Gadget Finder</a></li>
-											<li><a href="#">Green Tree Express</a></li>
-											<li><a href="#">Green Tree Pro</a></li>
-											<li><a href="#">Wobbler 3.0</a></li>
-											<li><a href="#">Coolkid</a></li>
-										</ul>
+										<a>Hakkimizda<a>
 									</div>
 									<div>
-										<h4>Education</h4>
-										<ul>
-											<li><a href="#">Learn Thai</a></li>
-											<li><a href="#">Math Genius</a></li>
-											<li><a href="#">Chemokid</a></li>
-										</ul>
-										<h4>Professionals</h4>
-										<ul>
-											<li><a href="#">Success 1.0</a></li>
-											<li><a href="#">Moneymaker</a></li>
-										</ul>
+										<a>Nasil Uye Olunur?</a>
+									</div>
+									<div>
+										<a>Merkezi Yonetim</a>
 									</div>
 								</div><!-- /cbp-hrsub-inner -->
 							</div><!-- /cbp-hrsub -->
@@ -124,31 +57,13 @@
 							<div class="cbp-hrsub">
 								<div class="cbp-hrsub-inner"> 
 									<div>
-										<h4>Learning &amp; Games</h4>
-										<ul>
-											<li><a href="#">Catch the Bullet</a></li>
-											<li><a href="#">Snoopydoo</a></li>
-											<li><a href="#">Fallen Angel</a></li>
-											<li><a href="#">Sui Maker</a></li>
-											<li><a href="#">Wave Master</a></li>
-											<li><a href="#">Golf Pro</a></li>
-										</ul>
-										<h4>Utilities</h4>
-										<ul>
-											<li><a href="#">Gadget Finder</a></li>
-											<li><a href="#">Green Tree Express</a></li>
-										</ul>
+										<a>Hakkimizda<a>
 									</div>
 									<div>
-										<h4>Entertainment</h4>
-										<ul>
-											<li><a href="#">Gadget Finder</a></li>
-											<li><a href="#">Green Tree Express</a></li>
-											<li><a href="#">Green Tree Pro</a></li>
-											<li><a href="#">Holy Cannoli</a></li>
-											<li><a href="#">Wobbler 3.0</a></li>
-											<li><a href="#">Coolkid</a></li>
-										</ul>
+										<a>Nasil Uye Olunur?</a>
+									</div>
+									<div>
+										<a>Merkezi Yonetim</a>
 									</div>
 								</div><!-- /cbp-hrsub-inner -->
 							</div><!-- /cbp-hrsub -->
@@ -158,40 +73,16 @@
 							<div class="cbp-hrsub">
 								<div class="cbp-hrsub-inner"> 
 									<div>
-										<h4>Utilities</h4>
-										<ul>
-											<li><a href="#">Green Tree Pro</a></li>
-											<li><a href="#">Wobbler 3.0</a></li>
-											<li><a href="#">Coolkid</a></li>
-										</ul>
-										<h4>Education</h4>
-										<ul>
-											<li><a href="#">Learn Thai</a></li>
-											<li><a href="#">Math Genius</a></li>
-											<li><a href="#">Chemokid</a></li>
-										</ul>
+										<a>Hakkimizda<a>
 									</div>
 									<div>
-										<h4>Professionals</h4>
-										<ul>
-											<li><a href="#">Success 1.0</a></li>
-											<li><a href="#">Moneymaker</a></li>
-										</ul>
+										<a>Nasil Uye Olunur?</a>
 									</div>
 									<div>
-										<h4>Learning &amp; Games</h4>
-										<ul>
-											<li><a href="#">Catch the Bullet</a></li>
-											<li><a href="#">Snoopydoo</a></li>
-											<li><a href="#">Fallen Angel</a></li>
-											<li><a href="#">Sui Maker</a></li>
-											<li><a href="#">Wave Master</a></li>
-											<li><a href="#">Golf Pro</a></li>
-										</ul>
+										<a>Merkezi Yonetim</a>
 									</div>
 								</div><!-- /cbp-hrsub-inner -->
 							</div><!-- /cbp-hrsub -->
 						</li>
-						<li><button class="btn btn-primary">Gonullu Portali</button></li>
 					</ul>
 				</nav>
