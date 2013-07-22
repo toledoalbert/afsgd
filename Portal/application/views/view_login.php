04e121a23da789c5789da726b120d4c71b9a5372
toledoalbert <toledoalbert@gmail.com>
2013-07-22 01:23:21 -0400

    sign in background changed

--- Portal/application/views/view_login.php
+++ Portal/application/views/view_login.php
@@ -1,4 +1,4 @@
-<!-- <div style="width: 100%; height:100%; background: url(<?php echo base_url();?>/img/blueworld.jpg);">-->
+<div style="width: 100%; height:100%; min-height: 50em ; background: url(<?php echo base_url();?>/img/blueworld.jpg);">
 <div class="container">
 
 	<div class="row" style="margin-top:10%;">
