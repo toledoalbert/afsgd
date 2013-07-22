7d53b27fac90e23e61ad4c428a878f85bd1cf56d
Albert Toledo <toledoalbert@gmail.com>
2013-07-22 01:10:46 -0400

    minor changes

--- Portal/application/views/view_login.php
+++ Portal/application/views/view_login.php
@@ -1,4 +1,4 @@
-<!--<div style="width: 100%; height:100%; background: url(<?php echo base_url();?>/img/blueworld.jpg);"> -->
+<!-- <div style="width: 100%; height:100%; background: url(<?php echo base_url();?>/img/blueworld.jpg);">-->
 <div class="container">
 
 	<div class="row" style="margin-top:10%;">
@@ -6,7 +6,7 @@
 			<img src="<?php echo base_url(); ?>/img/portalLogo.png" />
 			<legend>Uye Girisi</legend>
           	<div class="alert alert-error">
-                <a class="close" data-dismiss="alert" href="#">×</a>Incorrect Username or Password!
+                <a class="close" data-dismiss="alert" href="#">×</a>Hatali E-posta veya Sifre!
             </div>
 			<form method="POST" action="" accept-charset="UTF-8">
 			<input type="text" id="username" class="span4" name="username" placeholder="E-posta">
