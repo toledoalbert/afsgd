61b53cbabbebe31cb27c2fcb34a8cdea3be9add6
toledoalbert <toledoalbert@gmail.com>
2013-08-22 17:43:59 +0300

    social media icons added as addthis and footer

--- application/views/site_footer.php
+++ application/views/site_footer.php
@@ -1,78 +1,64 @@
 </div>
 <div class="invisible-seperator"></div>
 <div id="footer" class="container visible-desktop"> 
-<div id="footer-inside" class="row-fluid text-center">
-        <div class="span12">
-          <div class="span2" style="width: 18%;">
-            <ul class="unstyled">
-              <li>DERNEK<li>
-              <li><a href="hakkimizda">Hakkımızda</a></li>
-              <li><a href="kurulus">Kuruluş Hikayesi</a></li>
-              <li><a href="myk">Merkez Yönetim Kurulu</a></li>
-              <li><a href="nasil">Nasıl Üye Olunur?</a></li>
-            </ul>
-          </div>
-          <div class="span2" style="width: 18%;">
-            <ul class="unstyled">
-              <li>DUYURULAR<li>
-              <li><a href="merkez">Merkezi Yönetim Kurulu</a></li>
-              <li><a href="ankara">Ankara Şubesi</a></li>
-              <li><a href="istanbul">İstanbul Şubesi</a></li>
-              <li><a href="#ankara">Ankara Şubesi</a></li>              
-              <li><a href="haberler">Haberler</a></li>              
-            </ul>
-          </div>
-          <div class="span2" style="width: 18%;">
-            <ul class="unstyled">
-              <li>PROJELER VE ETKİNLİKLER<li>
-              <li><a href="afsd">AFS'D</a></li>
-              <li><a href="aka">Ankara Kültür Avı</a></li>
-              <li><a href="chapex">Chapex</a></li>
-              <li><a href="festikült">Fest-i Kult</a></li>              
-              <li><a href="gülencocuk">Gülen Çocuk Festivali</a></li>              
-              <li><a href="ika">İstanbul Kültür Avı</a></li>              
-              <li><a href="kamplar">Oryantasyon Kampları</a></li>              
-            </ul>
-          </div>
-          <div class="span2" style="width: 18%;">
-            <ul class="unstyled">
-              <li>ADRES<li>
-              <li><a target="_blank" href="https://www.google.com/maps?q=Me%C5%9Frutiyet+Mh.+Samanyolu+Sk+9-41,+34400+%C5%9Ei%C5%9Fli%2F%C4%B0stanbul&ie=UTF8&sll=40.4313684,-79.9805005&sspn=0.20072197450695625,0.3516235464532716&t=m&dg=opt&hnear=Me%C5%9Frutiyet+Mh.,+Samanyolu+Sk+No:9,+%C5%9Ei%C5%9Fli,+Turkey&z=17">Samanyolu Sok. Ümit Han. No:36 D:9 Osmanbey – Şişli</a></li>  
-              <li><a href="tel: +902123432979">0212 343 29 79</a><li>          
-              <li><a href="mailto:info@afsgd.org">info@afsgd.org</a><li>          
-            </ul>
-          </div>
-          <div class="span2" style="width: 18%;">
-            <ul class="unstyled">
-              <li>Sosyal Medya</li>
-              <li><a style="color: #3464FF;" href="http://facebook.com/AFSGD">Facebook</a><li>
-              <li><a style="color: #00D3F0;" href="http://twitter.com/AFSGD">Twitter</a></li> 
-              <li><a style="color: #DF0101;" href="http://youtube.com/AFSGonulluleriDer">Youtube</a></li>             
-            </ul>
-          </div>
-          
-        </div>
-      </div>
-      <hr>
-      <div class="row-fluid">
-        <div id="footer-bottom" class="span12">
-            <p class="muted text-center">AFS Gonulluleri Dernegi</p>
-        </div>
-        </div>
-      </div>
-  </div>
+  <div id="footer-inside" class="row-fluid text-center">
+    <div class="span12">
 
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url();?>scripts/Bootstrap/bootstrap.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url();?>scripts/charitywater1.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-<script src="<?php echo base_url();?>scripts/horizontalNavbarBlueHover.js"></script>
-<script>
+      <div class="span2" style="width: 18%;">
+        <ul class="unstyled">
+          <li>ADRES<li>
+            <li><a target="_blank" href="https://www.google.com/maps?q=Me%C5%9Frutiyet+Mh.+Samanyolu+Sk+9-41,+34400+%C5%9Ei%C5%9Fli%2F%C4%B0stanbul&ie=UTF8&sll=40.4313684,-79.9805005&sspn=0.20072197450695625,0.3516235464532716&t=m&dg=opt&hnear=Me%C5%9Frutiyet+Mh.,+Samanyolu+Sk+No:9,+%C5%9Ei%C5%9Fli,+Turkey&z=17">Samanyolu Sok. Ümit Han. No:36 D:9 Osmanbey – Şişli/ İstanbul</a></li>  
+            <li><a href="tel: +902123432979">0212 343 29 79</a><li>          
+              <li><a href="mailto:info@afsgd.org">info@afsgd.org</a><li>          
+              </ul>
+            </div>
+
+            <div class="span1 offset5">
+              <ul class="unstyled">
+
+                <li><iframe src="//www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.facebook.com%2FAFSGD&amp;width=150&amp;height=21&amp;colorscheme=light&amp;layout=button_count&amp;action=like&amp;show_faces=false&amp;send=false&amp;appId=187935111274116" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:85px; height:21px;" allowTransparency="true"></iframe></li>
+
+                 <li><a href="https://twitter.com/AFSGD" class="twitter-follow-button" data-show-count="false" data-lang="tr" data-size="large">Takip et: @AFSGD</a>
+                  <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script></li>
+
+              </ul>
+            </div>
+
+            </div>
+          </div>
+          <hr>
+          <div class="row-fluid">
+            <div id="footer-bottom" class="span12">
+              <p class="muted text-center">AFS Gonulluleri Dernegi</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- AddThis Button BEGIN -->
+<div class="addthis_toolbox addthis_floating_style addthis_32x32_style" style="left:0px;top:200px; background-color: transparent;">
+  <h5>Paylaş</h5>
+<a class="addthis_button_facebook"><img src="<?php echo base_url(); ?>img/facebook.png" alt=""></a>
+<a class="addthis_button_twitter"><img src="<?php echo base_url(); ?>img/twitter.png" alt=""></a>
+<a class="addthis_button_google_plusone_share"><img src="<?php echo base_url(); ?>img/google.png" alt=""></a>
+<a class="addthis_button_email"><img src="<?php echo base_url(); ?>img/email.png" alt=""></a>
+
+</div>
+<script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-52161c9a0d072af2"></script>
+<!-- AddThis Button END -->
+
+      <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+      <script type="text/javascript" src="<?php echo base_url();?>scripts/Bootstrap/bootstrap.min.js"></script>
+      <script type="text/javascript" src="<?php echo base_url();?>scripts/charitywater1.js"></script>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+      <script src="<?php echo base_url();?>scripts/horizontalNavbarBlueHover.js"></script>
+      <script>
       $(function() {
         cbpHorizontalMenu.init();
       });
-</script>
-</div>
-</body>
+      </script>
+    </div>
+  </body>
 
-</html>
+  </html>
