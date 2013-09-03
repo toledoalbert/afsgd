0822cee196439920f2655c98f5fef8e4bb289bbe
srle <sercanselvi35@gmail.com>
2013-09-03 17:04:43 +0300

    Update view_myk.php

--- application/views/view_myk.php
+++ application/views/view_myk.php
@@ -8,50 +8,51 @@
 <h1>Merkez Yonetim Kurulu</h1>
 <h3>Yonetim Kurulu</h3>
 <ul>
-	<li>Levent Afsar <span class="label label-success">Baskan</span> </li>
-	<li>Selva Aydiner <span class="label label-success">Baskan Yardimcisi</span> </li>
-	<li>Sinemis Candemir <span class="label label-success">Baskan Yardimcisi</span> </li>
-	<li>Ayse Duygu Kutluay <span class="label label-success">Sekreter</span> </li>
-	<li>Barkin Aykanat <span class="label label-success">Sayman</span> </li>
-	<li>Pinar Kose <span class="label label-success">Uye</span> </li>
-	<li>Merve Yurdakul <span class="label label-success">Uye</span> </li>
+	<li><span class="label label-success">Baskan</span> Levent Afsar  </li>
+	<li><span class="label label-success">Baskan Yardimcisi</span> Selva Aydıner  </li>
+	<li><span class="label label-success">Baskan Yardimcisi</span> Sinemis Candemir </li>
+	<li><span class="label label-success">Sekreter</span> Ayşe Duygu Kutluay </li>
+	<li><span class="label label-success">Sayman</span> Barkın Aykanat </li>
+	<li><span class="label label-success">Üye</span> Pınar Köse</li>
+	<li><span class="label label-success">Üye</span> Merve Yurdakul </li>
 </ul>
 </div>
 
-<div class="span6">
-<h3>Yonetim Kurulu Yedek</h3>
-<ul>
-	<li>Sila Yucesoy</li>
-	<li>Mustafa Kemal Engin </li>
-	<li>Izzet Goksel </li>
-	<li>Ceren Kartari Urenden </li>
-	<li>Mehmet Haciomeroglu </li>
-	<li>Osman Kemal Sozer </li>
-	<li>Meral Tasar</li>
-</ul>
-</div>
-</div>
-
-<div class="row row-fluid">
-
+<br><br><br>
 <div class="span6">
 <h3>Denetleme Kurulu (Asil ve Yedek)</h3>
 <ul>
 	<li>Efe Songun</li>
 	<li>Belgin Acar </li>
-	<li>Ozge Bilici Sekerkiran</li>
+	<li>Özge Bilici Şekerkıran</li>
 	<li>Duygu Erkek </li>
-	<li>Inanc Vardar</li>
-	<li>Muhammed Cagri Kupeli </li>
+	<li>İnanc Vardar</li>
+	<li>Muhammed Çağrı Küpeli </li>
+</ul>
+</div>
+</div>
+<div class="row row-fluid">
+
+<div class="span6">
+<h3>Yönetim Kurulu Yedek</h3>
+<ul>
+	<li>Sıla Yücesoy</li>
+	<li>Mustafa Kemal Engin </li>
+	<li>İzzet Göksel </li>
+	<li>Ceren Kartari Urenden </li>
+	<li>Mehmet Hacıomeroğlu </li>
+	<li>Osman Kemal Sözer </li>
+	<li>Meral Tasar</li>
 </ul>
 </div>
 
+
 <div class="span6">
-<h3>Danisma Kurulu</h3>
+<h3>Danışma Kurulu</h3>
 <ul>
-	<li>Ozgur Akman</li>
-	<li>Burak Tarim </li>
-	<li>Veli Tan Kirtis</li>
+	<li>Veli Tan Kirtiş</li>
+	<li>Özgür Akman</li>
+	<li>Burak Tarım </li>
 </ul>
 </div>
 </div>
